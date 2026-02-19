4b4122ba81e26736a60ccf1f93c102eb7193aa35
kingtender <kingtender@gmail.com>
2026-02-18 17:40:47 -0800

    Update prompts and description in Chat component

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ import Image from "next/image";
 
 const examples = [
   "What should I do to live a better life?",
-  "do a first draft of a poem about coral",
+  "Does writing discover what you think or create what you think when thinking about coral?",
   "Is this something you're navigating right now?",
 ];
 
@@ -83,7 +83,7 @@ export default function Chat() {
               className="h-20 w-20"
             />
             <h1 className="text-lg font-semibold text-black">
-              Hi, I'm a fine tuned LLM emulating an Annoying Poet.
+              Hi, I'm a fine tuned LLM emulating a well intentioned poet, into marxism and the natural world.
             </h1>
             <p className="text-gray-500">
               I'm part of a series of computational experiments taught by{" "}
